d048e40c1e50064d094c34bfac8aa3f52940fefb
Manu <manueljesusrejonsantana@gmail.com>
2025-07-10 20:28:52 -0600

    practique objetos y for

---
+++ js/for.js
@@ -0,0 +1,32 @@
+/*for (let index = 0; index < 11; index++) {
+    console.log(index)
+}*/
+
+/*for (let index = 0; index < 11; index++) {
+    if (index > 5) break
+    console.log(index)
+}*/
+
+/*for (let index = 11; index > 0; index--) {
+    console.log(index)
+}*/
+
+/*for (let index = 0; index < 11; index++) {
+    if (index === 5) continue
+    console.log(index)
+}*/
+
+/*let paises = ["Mex", "usa", "cad", "br", "col"];
+
+for (let index = 0; index < 5; index++) {
+    console.log(paises[index])
+}*/
+
+
+//for anidado
+for (let index = 0; index < 5; index++) {
+    let caja = index + ".";
+    for (let indice = 0; indice < 5; indice++) {
+        console.log(caja + indice)
+    }
+}
---
+++ js/objetos.js
@@ -0,0 +1,23 @@
+//Array multidimensional
+/*
+let mascotas = ["perros", "gatos", "ratones", ["pez", "perico"]];
+console.log(mascotas[3][0]);*/
+
+let persona = {
+    nombre: "Manuel",
+    edad: 30,
+    mascotas: ["bob", "micaela"],
+    familia: {
+        papa: "Manuel",
+        mama: "Rosa",
+        tios: ["Manolo", "Pedro"]
+    }
+}
+console.log(persona.edad)
+console.log(persona.mascotas[1])
+persona.correo = "manuel@correo.com"
+console.log(persona)
+console.log(persona.familia.papa)
+console.log(persona.familia.tios[1])
+delete persona.familia.mama;
+console.log(persona)
